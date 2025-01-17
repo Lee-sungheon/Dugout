460efa0199f097a759b76ad3b561e20afa9ac26a
sunhyeong <parksunhyeong529@gmail.com>
2025-01-17 15:34:40 +0900

    [FEAT]구단페이지이동

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,15 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "@/pages/HomeView.vue";
+import Kia from "../pages/Kia.vue";
+import Samsung from "../pages/Samsung.vue";
+import LG from "../pages/Lg.vue";
+import Doosan from "../pages/Doosan.vue";
+import KT from "../pages/Kt.vue";
+import SSG from "../pages/Ssg.vue";
+import Lotte from "../pages/Lotte.vue";
+import Hanwha from "../pages/Hanhwa.vue";
+import NC from "../pages/Nc.vue";
+import Kiwoom from "../pages/Kiwoom.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -27,6 +37,16 @@ const router = createRouter({
 
       component: () => import("../pages/AboutView.vue"),
     },
+    { path: "/kia", component: Kia },
+    { path: "/samsung", component: Samsung },
+    { path: "/lg", component: LG },
+    { path: "/doosan", component: Doosan },
+    { path: "/kt", component: KT },
+    { path: "/ssg", component: SSG },
+    { path: "/lotte", component: Lotte },
+    { path: "/hanhwa", component: Hanwha },
+    { path: "/nc", component: NC },
+    { path: "/kiwoom", component: Kiwoom },
   ],
 });
 
