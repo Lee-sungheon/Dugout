34c764ee71a2cad7a429861ac390b71b6ecf87a5
sunhyeong <parksunhyeong529@gmail.com>
2025-01-17 14:26:43 +0900

    색상추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        white01: "#FAFAFA",
+        white02: "#EEEEEE",
+        black01: "#0A0A0A",
+        balck02: "#1E1E1E",
+        gray01: "#B1B1B1",
+        gray03: "#515151",
+        gray02: "#919191",
+      },
+    },
   },
   plugins: [],
 };
