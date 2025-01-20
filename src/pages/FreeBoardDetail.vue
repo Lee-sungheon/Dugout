d7df1a4c3e8d49bf6b30a0686c340a662a230371
kangsuyeong <ekdh16@naver.com>
2025-01-20 16:08:44 +0900

    [REFACTOR] 게시물 헤더 컴포넌트 분리

--- src/pages/FreeBoardDetail.vue
+++ src/pages/FreeBoardDetail.vue
@@ -1,11 +1,12 @@
 <script setup>
 import backIcon from "@/assets/icons/back.svg";
 import CommentSection from "@/components/CommentSection.vue";
+import PostHeader from "@/components/PostHeader.vue";
 </script>
 <template>
   <div>
     <!-- 뒤로가기 -->
-    <div class="border-2 border-green-500 mb-[50px]">
+    <div class="border-2 border-green-500 mb-[50px] flex">
       <button>
         <img :src="backIcon" alt="뒤로가기 아이콘" />
       </button>
@@ -13,35 +14,7 @@ import CommentSection from "@/components/CommentSection.vue";
     <!-- 상세페이지 -->
     <div class="flex flex-col gap-[50px]">
       <!-- 상세 페이지 정보 -->
-      <div class="flex flex-col gap-[10px] pb-5 border-b border-white02">
-        <!-- 제목 -->
-        <div class="w-full border border-red-500">
-          <span class="text-2xl font-bold"
-            >작년 첫 직관! 난 개막만을 기다려...</span
-          >
-        </div>
-        <!-- 유저 정보 / 수정 삭제 -->
-        <div class="flex items-center justify-between border border-red-500">
-          <!-- 유저정보 -->
-          <div class="flex items-center gap-[10px] border border-blue-500">
-            <img
-              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQDr6SB_fokX3TJBAFcrIisQ_YGwVVO0F8PCw&s"
-              alt="유저 프로필"
-              class="w-[25px] h-[25px] rounded-full"
-            />
-            <span class="text-xs text-gray03">닉네임</span>
-            <span class="text-xs text-gray02">4시간 전</span>
-          </div>
-          <!-- 수정 삭제 버튼 -->
-          <div
-            class="flex text-xs text-gray02 gap-[4px] border border-blue-500"
-          >
-            <button class="hover:text-gray03">수정</button>
-            <span>|</span>
-            <button class="hover:text-gray03">삭제</button>
-          </div>
-        </div>
-      </div>
+      <PostHeader />
       <!-- 게시물 내용 -->
       <div
         class="border-x border-blue-500 py-[50px] flex border-b border-b-gray01 flex-col items-center"
