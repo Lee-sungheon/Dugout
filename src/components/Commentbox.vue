4a62303f5fa8856651a257ad35a33d5c7c56a0a3
강수영 <suyeong@gangsuyeong-ui-MacBookPro.local>
2025-01-20 11:53:42 +0900

    [DESIGN] 자유게시판 상세페이지 퍼블리싱

---
+++ src/components/Commentbox.vue
@@ -0,0 +1,26 @@
+<script setup></script>
+<template>
+  <div class="pb-5 border-b border-white02">
+    <div class="flex items-center justify-between">
+      <div class="flex gap-[10px] items-center">
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQDr6SB_fokX3TJBAFcrIisQ_YGwVVO0F8PCw&s"
+          alt="유저 프로필"
+          class="w-[35px] h-[35px] rounded-full"
+        />
+        <span class="text-sm font-bold text-gray03">닉네임</span>
+        <span class="text-xs text-gray02">1분 전</span>
+      </div>
+      <div class="flex text-xs text-gray02 gap-[4px] border border-blue-500">
+        <button class="hover:text-gray03">수정</button>
+        <span>|</span>
+        <button class="hover:text-gray03">삭제</button>
+      </div>
+    </div>
+    <div class="pl-[45px]">
+      <span class="text-[#515151]">댓글 테스트 중입니다.</span>
+    </div>
+  </div>
+</template>
+
+<style ßscoped></style>
