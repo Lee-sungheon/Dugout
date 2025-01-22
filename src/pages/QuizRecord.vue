d7dd85b2780a27e348f8c8f5a9de5a84ea03d49a
ddongguri-bing <ckdalssnsk@naver.com>
2025-01-23 00:10:20 +0900

    [FEAT] 야구 퀴즈 기본 레이아웃 추가

---
+++ src/pages/QuizRecord.vue
@@ -0,0 +1,13 @@
+<script setup>
+import Monitor from "@/assets/images/gamemonitor.svg";
+</script>
+<template>
+  <div class="relative mx-auto w-max">
+    <img :src="Monitor" class="h-[670px]" />
+    <div
+      class="absolute top-[109px] left-1/2 translate-x-[-49%] w-full px-[89px] flex justify-between">
+      <
+    </div>
+  </div>
+</template>
+<style scoped></style>
