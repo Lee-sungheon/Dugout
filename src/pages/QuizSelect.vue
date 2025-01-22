b2d0989e78da07bd083cccf68f0735830df0db70
sunhyeong <parksunhyeong529@gmail.com>
2025-01-22 17:04:10 +0900

    [DESIGN]퀴즈선택퍼블리싱

--- src/pages/QuizSelect.vue
+++ src/pages/QuizSelect.vue
@@ -4,8 +4,11 @@ import InfoQuiz from "@/assets/images/info_quiz_card.svg";
 import InfoQuizGreen from "@/assets/images/info_quiz_card_green.svg";
 import PlayerQuiz from "@/assets/images/player_quiz_card.svg";
 import PlayerQuizGreen from "@/assets/images/player_quiz_card_green.svg";
-import recordQuiz from "@/assets/images/record_quiz_card.svg";
-import recordQuizGreen from "@/assets/images/record_quiz_card_green.svg";
+import RecordQuiz from "@/assets/images/record_quiz_card.svg";
+import RecordQuizGreen from "@/assets/images/record_quiz_card_green.svg";
+import PlayBtn from "@/assets/images/play_btn.svg";
+
+//개발할 때 선택하면 green 컬러로 카드, 텍스트 바뀌는 로직 구현
 </script>
 <template>
   <div class="relative w-max mx-auto">
@@ -13,10 +16,34 @@ import recordQuizGreen from "@/assets/images/record_quiz_card_green.svg";
     <div
       class="absolute top-[109px] left-1/2 translate-x-[-49%] w-full px-[89px] flex justify-between"
     >
-      <img :src="InfoQuiz" class="h-[217px] flex-1 object-contain" />
-      <img :src="PlayerQuiz" class="h-[217px] flex-1 object-contain" />
-      <img :src="recordQuiz" class="h-[217px] flex-1 object-contain" />
+      <button
+        class="flex flex-col justify-center items-center gap-[16px] cursor-pointer"
+      >
+        <img :src="InfoQuiz" class="h-[217px] flex-1 object-contain" />
+        <span class="font-Galmuri11 text-gameGreen font-bold"
+          >야구 상식 퀴즈</span
+        >
+      </button>
+      <button
+        class="flex flex-col justify-center items-center gap-[16px] cursor-pointer"
+      >
+        <img :src="PlayerQuiz" class="h-[217px] flex-1 object-contain" />
+        <span class="font-Galmuri11 text-gameGreen font-bold">선수 맞추기</span>
+      </button>
+      <button
+        class="flex flex-col justify-center items-center gap-[16px] cursor-pointer"
+      >
+        <img :src="RecordQuiz" class="h-[217px] flex-1 object-contain" />
+        <span class="font-Galmuri11 text-gameGreen font-bold"
+          >야구 기록 퀴즈</span
+        >
+      </button>
     </div>
+    <button
+      class="absolute bottom-[140px] left-1/2 translate-x-[-45%] cursor-pointer"
+    >
+      <img :src="PlayBtn" />
+    </button>
   </div>
 </template>
 <style scoped></style>
