8d525b66bc034176569bd293339a9ef7054ca392
ddongguri-bing <ckdalssnsk@naver.com>
2025-01-23 00:09:34 +0900

    [FIX] 라우팅 경로 변경

--- src/pages/Game.vue
+++ src/pages/Game.vue
@@ -6,13 +6,13 @@ const router = useRouter();
 
 // 각 버튼 클릭 시 경로 이동 함수
 const goToBaseBallGame = () => {
-  router.push("/game/BaseBallGame");
+  router.push("/game/baseballgame");
 };
 const goToBaseBallQuiz = () => {
-  router.push("/game/BaseBallQuiz");
+  router.push("/game/quizselect");
 };
 const goToBaseBallTMT = () => {
-  router.push("/game/BaseBallTMT");
+  router.push("/game/tournamentselect");
 };
 </script>
 
@@ -23,40 +23,33 @@ const goToBaseBallTMT = () => {
     <!-- 100% 검정색 배경 -->
     <div
       class="absolute top-0 left-0 w-full h-full z-0"
-      style="background-color: #070707"
-    ></div>
+      style="background-color: #070707"></div>
 
     <!-- 모니터 레이어 -->
     <div
-      class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[688px] h-[670px] z-10"
-    >
+      class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[688px] h-[670px] z-10">
       <img
         src="/src/assets/icons/game-monitor.svg"
         alt="Monitor Background"
-        class="w-full h-full object-contain"
-      />
+        class="w-full h-full object-contain" />
     </div>
 
     <!-- 20% 투명 배경 -->
     <div
       class="absolute top-0 left-0 w-full h-full z-20"
-      style="background-color: rgba(211, 211, 211, 0.2)"
-    ></div>
+      style="background-color: rgba(211, 211, 211, 0.2)"></div>
 
     <!-- 버튼 레이어 -->
     <div
-      class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex justify-center gap-[200px] z-30"
-    >
+      class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex justify-center gap-[200px] z-30">
       <!-- 야구 게임 버튼 -->
       <div class="flex flex-col items-center" @click="goToBaseBallGame">
         <div
-          class="w-[80px] h-[80px] flex justify-center items-center cursor-pointer"
-        >
+          class="w-[80px] h-[80px] flex justify-center items-center cursor-pointer">
           <img
             src="/src/assets/icons/bt_baseBallGame.svg"
             alt="야구 게임"
-            class="w-20 h-20"
-          />
+            class="w-20 h-20" />
         </div>
         <span class="mt-2 text-white text-lg">야구 게임</span>
       </div>
@@ -64,13 +57,11 @@ const goToBaseBallTMT = () => {
       <!-- 야구 퀴즈 버튼 -->
       <div class="flex flex-col items-center" @click="goToBaseBallQuiz">
         <div
-          class="w-[80px] h-[80px] flex justify-center items-center cursor-pointer"
-        >
+          class="w-[80px] h-[80px] flex justify-center items-center cursor-pointer">
           <img
             src="/src/assets/icons/bt_baseBallQuiz.svg"
             alt="야구 퀴즈"
-            class="w-20 h-20"
-          />
+            class="w-20 h-20" />
         </div>
         <span class="mt-2 text-[#0bf89f] text-lg">야구 퀴즈</span>
       </div>
@@ -78,13 +69,11 @@ const goToBaseBallTMT = () => {
       <!-- 토너먼트 버튼 -->
       <div class="flex flex-col items-center" @click="goToBaseBallTMT">
         <div
-          class="w-[80px] h-[80px] flex justify-center items-center cursor-pointer"
-        >
+          class="w-[80px] h-[80px] flex justify-center items-center cursor-pointer">
           <img
             src="/src/assets/icons/bt_baseBallTMT.svg"
             alt="토너먼트"
-            class="w-20 h-20"
-          />
+            class="w-20 h-20" />
         </div>
         <span class="mt-2 text-white text-lg">토너먼트</span>
       </div>
