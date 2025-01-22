5e15b3432a9010f5d183f385cd80c34b84e7033f
sunhyeong <parksunhyeong529@gmail.com>
2025-01-22 16:49:31 +0900

    퀴즈셀렉트작업중

--- src/constants/index.js
+++ src/constants/index.js
@@ -8,6 +8,12 @@ import lotteLogo from "@/assets/images/logo_lotte.svg";
 import hanhwaLogo from "@/assets/images/logo_hanhwa.svg";
 import ncLogo from "@/assets/images/logo_nc.svg";
 import kiwoomLogo from "@/assets/images/logo_kiwoom.svg";
+import gameIcon from "@/assets/images/baseballgame_logo.svg";
+import quizIcon from "@/assets/images/quiz_logo.svg";
+import tournamentIcon from "@/assets/images/tournament-logo.svg";
+import gameIconBlack from "@/assets/images/baseballgame_logo_black.svg";
+import quizIconBlack from "@/assets/images/quiz_logo_black.svg";
+import tournamentIconBlack from "@/assets/images/tournament_logo_black.svg";
 
 export const teamList = [
   { name: "kia", path: "/kia/freeboard", logo: kiaLogo },
@@ -20,4 +26,25 @@ export const teamList = [
   { name: "hanhwa", path: "/hanhwa/freeboard", logo: hanhwaLogo },
   { name: "nc", path: "/nc/freeboard", logo: ncLogo },
   { name: "kiwoom", path: "/kiwoom/freeboard", logo: kiwoomLogo },
+];
+
+export const gameList = [
+  {
+    name: "야구 게임",
+    path: "baseballgame",
+    icon: gameIcon,
+    iconBlack: gameIconBlack,
+  },
+  {
+    name: "야구 퀴즈",
+    path: "quizselect",
+    icon: quizIcon,
+    iconBlack: quizIconBlack,
+  },
+  {
+    name: "야구 토너먼트",
+    path: "tournamentselect",
+    icon: tournamentIcon,
+    iconBlack: tournamentIconBlack,
+  },
 ];
---
+++ src/pages/QuizSelect.vue
@@ -0,0 +1,22 @@
+<script setup>
+import Monitor from "@/assets/images/gamemonitor.svg";
+import InfoQuiz from "@/assets/images/info_quiz_card.svg";
+import InfoQuizGreen from "@/assets/images/info_quiz_card_green.svg";
+import PlayerQuiz from "@/assets/images/player_quiz_card.svg";
+import PlayerQuizGreen from "@/assets/images/player_quiz_card_green.svg";
+import recordQuiz from "@/assets/images/record_quiz_card.svg";
+import recordQuizGreen from "@/assets/images/record_quiz_card_green.svg";
+</script>
+<template>
+  <div class="relative w-max mx-auto">
+    <img :src="Monitor" class="h-[670px]" />
+    <div
+      class="absolute top-[109px] left-1/2 translate-x-[-49%] w-full px-[89px] flex justify-between"
+    >
+      <img :src="InfoQuiz" class="h-[217px] flex-1 object-contain" />
+      <img :src="PlayerQuiz" class="h-[217px] flex-1 object-contain" />
+      <img :src="recordQuiz" class="h-[217px] flex-1 object-contain" />
+    </div>
+  </div>
+</template>
+<style scoped></style>
