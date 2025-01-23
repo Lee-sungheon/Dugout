045c36005e0d0516076788235fe5a2c279e041f4
ddongguri-bing <ckdalssnsk@naver.com>
2025-01-23 10:26:20 +0900

    [FEAT] 야구 토너먼트 라우팅 및 시작 페이지 구현

--- src/pages/TournamentSelect.vue
+++ src/pages/TournamentSelect.vue
@@ -7,41 +7,89 @@ import GoodPlayerGreen from "@/assets/images/good_player_card_green.svg";
 import HandsomePlayer from "@/assets/images/handsome_card.svg";
 import HandsomePlayerGreen from "@/assets/images/handsome_card_green.svg";
 import PlayBtn from "@/assets/images/play_btn.svg";
+import { useRouter } from "vue-router";
+import { ref } from "vue";
 
-//개발할 때 선택하면 green 컬러로 카드, 텍스트 바뀌는 로직 구현
+const router = useRouter();
+const selectedTournament = ref(null);
+
+// 퀴즈 선택 핸들러
+const selectTournament = (tournament) => {
+  selectedTournament.value = tournament;
+};
+
+// Start 버튼 핸들러
+const startTournament = () => {
+  if (selectedTournament.value) {
+    router.push(`/game/tournamentselect/${selectedTournament.value}`);
+  } else {
+    alert("토너먼트를 선택해주세요!");
+  }
+};
 </script>
 <template>
   <div class="relative w-max mx-auto">
     <img :src="Monitor" class="h-[670px]" />
     <div
-      class="absolute top-[109px] left-1/2 translate-x-[-49%] w-full px-[89px] flex justify-between"
-    >
+      class="absolute top-[109px] left-1/2 translate-x-[-49%] w-full px-[89px] flex justify-between">
       <button
-        class="flex flex-col justify-center items-center gap-[16px] cursor-pointer"
-      >
-        <img :src="BadPlayer" class="h-[217px] flex-1 object-contain" />
-        <span class="font-Galmuri11 text-gameGreen font-bold"
-          >야구 상식 퀴즈</span
+        @click="selectTournament('traitor')"
+        :class="selectedTournament === 'traitor' ? 'text-gameGreen' : ''"
+        class="flex flex-col justify-center items-center gap-[16px] cursor-pointer">
+        <img
+          :src="selectedTournament === 'traitor' ? BadPlayerGreen : BadPlayer"
+          class="h-[217px] flex-1 object-contain" />
+        <span
+          :class="
+            selectedTournament === 'traitor' ? 'text-gameGreen' : 'text-white01'
+          "
+          class="font-Galmuri11 font-bold"
+          >올해 최고의<br />
+          역적 월드컵</span
         >
       </button>
       <button
-        class="flex flex-col justify-center items-center gap-[16px] cursor-pointer"
-      >
-        <img :src="GoodPlayer" class="h-[217px] flex-1 object-contain" />
-        <span class="font-Galmuri11 text-gameGreen font-bold">선수 맞추기</span>
+        @click="selectTournament('hero')"
+        :class="selectedTournament === 'hero' ? 'text-gameGreen' : ''"
+        class="flex flex-col justify-center items-center gap-[16px] cursor-pointer">
+        <img
+          :src="selectedTournament === 'hero' ? GoodPlayerGreen : GoodPlayer"
+          class="h-[217px] flex-1 object-contain" />
+        <span
+          :class="
+            selectedTournament === 'hero' ? 'text-gameGreen' : 'text-white01'
+          "
+          class="font-Galmuri11 font-bold"
+          >올해 최고의 <br />
+          영웅 월드컵</span
+        >
       </button>
       <button
-        class="flex flex-col justify-center items-center gap-[16px] cursor-pointer"
-      >
-        <img :src="HandsomePlayer" class="h-[217px] flex-1 object-contain" />
-        <span class="font-Galmuri11 text-gameGreen font-bold"
-          >야구 기록 퀴즈</span
+        @click="selectTournament('handsome')"
+        :class="selectedTournament === 'handsome' ? 'text-gameGreen' : ''"
+        class="flex flex-col justify-center items-center gap-[16px] cursor-pointer">
+        <img
+          :src="
+            selectedTournament === 'handsome'
+              ? HandsomePlayerGreen
+              : HandsomePlayer
+          "
+          class="h-[217px] flex-1 object-contain" />
+        <span
+          :class="
+            selectedTournament === 'handsome'
+              ? 'text-gameGreen'
+              : 'text-white01'
+          "
+          class="font-Galmuri11 font-bold"
+          >올해 최고의 <br />
+          미남 월드컵</span
         >
       </button>
     </div>
     <button
-      class="absolute bottom-[140px] left-1/2 translate-x-[-45%] cursor-pointer"
-    >
+      @click="startTournament"
+      class="absolute bottom-[140px] left-1/2 translate-x-[-45%] cursor-pointer">
       <img :src="PlayBtn" />
     </button>
   </div>
