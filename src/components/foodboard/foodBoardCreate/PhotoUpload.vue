92b8687ac78049360bf701d5a0a1e25060073424
engsal <imaria0218@gmail.com>
2025-01-25 10:17:11 +0900

    [FEAT] 맛집 게시판 이미지 업로드 및 삭제 구현 -- QUIL오류 수정중

--- src/components/foodboard/foodBoardCreate/PhotoUpload.vue
+++ src/components/foodboard/foodBoardCreate/PhotoUpload.vue
@@ -1,76 +1,27 @@
 <script setup>
-import { ref } from "vue";
-import { supabase } from "@/supabase.js";
+import { useImageStore } from "@/stores/useImageStore";
 import DeletePhoto from "@/assets/icons/delete_photo.svg";
 import Camera from "@/assets/icons/camera.svg";
 
-// 이미지 URL을 저장할 배열
-const imageUrls = ref([null, null, null]); // 각 박스에 맞게 이미지 URL을 저장
-const errorMessage = ref(null); // 에러 메시지
+const imageStore = useImageStore();
 
-// 이미지 업로드 함수
-const handleImageUpload = async (index) => {
-  const fileInput = document.createElement("input");
-  fileInput.type = "file";
-  fileInput.accept = "image/*";
-
-  fileInput.onchange = async (e) => {
-    const target = e.target;
-    const file = target.files?.[0];
-
-    if (file) {
-      if (file.size > 3 * 1024 * 1024) {
-        errorMessage.value =
-          "파일 크기가 너무 큽니다. 3MB 이하의 파일을 선택해주세요.";
-        return;
-      } else {
-        errorMessage.value = null;
-      }
-
-      // 파일명을 URL-safe 방식으로 인코딩
-      const encodedFileName = encodeURIComponent(file.name);
-
-      // Supabase에 파일 업로드
-      const { data, error } = await supabase.storage
-        .from("images")
-        .upload(`public/${encodedFileName}`, file, {
-          cacheControl: "3600",
-          upsert: false,
-        });
-
-      if (error) {
-        console.error("업로드 오류:", error);
-      } else {
-        // 업로드된 이미지 URL 가져오기
-        const { publicURL, error: urlError } = supabase.storage
-          .from("images")
-          .getPublicUrl(`${data.path}`);
-
-        if (urlError) {
-          console.error("URL 가져오기 오류:", urlError);
-        } else {
-          console.log("이미지 URL:", publicURL);
-          // 해당 인덱스에 이미지 URL 저장
-          imageUrls.value[index] = publicURL;
-        }
-      }
-    }
-  };
-
-  fileInput.click();
+const handleFileChange = (event, index) => {
+  const file = event.target.files[0];
+  if (file) {
+    imageStore.uploadImage(file, index);
+  }
 };
 </script>
-
 <template>
   <section>
     <div class="flex gap-[30px]">
       <div
-        v-for="imageUrl in imageUrls"
+        v-for="(imageUrl, i) in imageStore.imageUrls"
         :key="i"
-        class="cursor-pointer w-full h-full aspect-square rounded-[10px] bg-white02"
+        class="cursor-pointer w-full h-full rounded-[10px] bg-white02 aspect-square"
       >
         <!-- 이미지를 업로드한 경우 -->
-        <label v-if="imageUrl" class="relative w-full h-full">
+        <div v-if="imageUrl" class="relative w-full h-full">
           <img
             :src="imageUrl"
             alt="이미 업로드된 이미지"
@@ -80,31 +31,30 @@ const handleImageUpload = async (index) => {
             :src="DeletePhoto"
             alt="이미 업로드된 이미지를 삭제"
             class="absolute top-[10px] right-[10px] cursor-pointer"
+            @click="imageStore.removeImage(i)"
           />
-        </label>
+        </div>
 
         <!-- 이미지를 업로드하지 않은 경우 -->
-        <div v-else>
-          <div
-            class="aspect-square w-full rounded-[10px] bg-white02 flex justify-center items-center"
-            @click="handleImageUpload(i)"
-          >
-            <img :src="Camera" />
-          </div>
-        </div>
+        <label
+          v-else
+          class="relative flex justify-center items-center w-full h-full"
+        >
+          <img
+            :src="Camera"
+            class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+          />
+          <input
+            type="file"
+            class="absolute inset-0 opacity-0 cursor-pointer"
+            @change="(event) => handleFileChange(event, i)"
+          />
+        </label>
       </div>
     </div>
-
     <!-- 에러 메시지 -->
-    <div v-if="errorMessage" class="text-red-500 mt-2">
-      {{ errorMessage }}
+    <div v-if="imageStore.errorMessage" class="text-red-500 mt-2">
+      {{ imageStore.errorMessage }}
     </div>
   </section>
 </template>
-
-<style scoped>
-/* input을 보이지 않게 하되 공간을 유지 */
-input.hidden {
-  display: none;
-}
-</style>
--- src/pages/FoodBoardCreate.vue
+++ src/pages/FoodBoardCreate.vue
@@ -1,39 +1,83 @@
 <script setup>
 import { ref } from "vue";
 import { useRoute, useRouter } from "vue-router";
-import { Delta } from "@vueup/vue-quill";
 import { QuillEditor } from "@vueup/vue-quill";
+import { useImageStore } from "@/stores/useImageStore";
+import { createRestaurantPost } from "@/api/supabase-api/restaurantPost";
+import { getCurrentUser } from "@/api/supabase-api/userInfo";
 import CreateHeader from "@/components/CreateHeader.vue";
 import MapSelectAndView from "@/components/foodboard/foodBoardCreate/MapSelectAndView.vue";
 import PhotoUpload from "@/components/foodboard/foodBoardCreate/PhotoUpload.vue";
 import TagsSelect from "@/components/foodboard/foodBoardCreate/TagsSelect.vue";
 import Modal from "@/components/common/Modal.vue";
+import { teamID } from "@/constants/index";
 
+const imageStore = useImageStore();
 const router = useRouter();
 const route = useRoute();
+const team = route.params.team;
 
 const modalmessage = ref("");
 const isModalVisible = ref(false);
 const inputTitle = ref("");
-const inputContent = ref(new Delta());
-// const deltaString = JSON.stringify(content.value);
-
+const inputContent = ref("");
+const selectedTags = ref([]);
 const messageList = [
   "작성했던 모든 내용은 저장되지 않습니다\n취소하시겠습니까?",
   "수정을 완료하시겠습니까?",
 ];
 
-const submitRestaurantPost = () => {
-  console.log("제출");
+const handleTagUpdate = (tags) => {
+  selectedTags.value = tags;
+  console.log("Selected Tags:", selectedTags.value);
 };
+
+const submitRestaurantPost = async () => {
+  console.log("제출");
+  const filteredImg = imageStore.filterNullImage();
+  const userData = await getCurrentUser();
+  console.log(userData);
+  console.log("selectedTags 값입니다", selectedTags);
+  console.log(
+    "selectedTags 값입니다",
+    inputContent,
+    "타이틀",
+    inputTitle,
+    "이미지",
+    filteredImg,
+    "태그",
+    selectedTags,
+    "내용",
+    inputContent
+  );
+  try {
+    // 서버에 HTML 콘텐츠를 전달하여 게시물 생성
+    await createRestaurantPost(
+      userData.id,
+      inputContent.value,
+      inputTitle.value,
+      filteredImg[0],
+      selectedTags.value,
+      teamID[team]
+    );
+    router.push(`/${team}/foodboard`);
+    console.log("포스팅 성공");
+  } catch (error) {
+    console.log("맛집 게시물 등록 실패", error);
+  }
+};
+
 const cancelRestaurantPost = () => {
   modalmessage.value = messageList[0];
   isModalVisible.value = true;
   console.log(route.params);
 };
-const onClickModalCancelBtn = () => {
-  const team = route.params.team;
-  console.log(route.params);
+
+const cancelModalWindow = () => {
+  isModalVisible.value = false;
+};
+
+const backToFoodboard = () => {
   router.push(`/${team}/foodboard`);
 };
 
@@ -46,13 +90,12 @@ const toolbarOptions = [
   // ['image'], // 이미지 버튼을 제거
 ];
 </script>
-
 <template>
   <Modal
     v-if="isModalVisible"
     :message="modalmessage"
-    :onCancel="onClickModalCancelBtn"
-    :onConfirm="submitRestaurantPost"
+    :onCancel="cancelModalWindow"
+    :onConfirm="backToFoodboard"
   />
   <section class="flex flex-col px-[50px] gap-[30px]">
     <CreateHeader
@@ -74,8 +117,8 @@ const toolbarOptions = [
       <MapSelectAndView />
       <div>
         <QuillEditor
-          v-model="inputContent"
-          contentType="delta"
+          v-model:value="inputContent"
+          contentType="html"
           :placeholder="'맛집을 마구 공유해주세요!\n맛집 사진은 최대 3개까지 업로드할 수 있습니다.'"
           theme="snow"
           :toolbar="toolbarOptions"
@@ -84,7 +127,7 @@ const toolbarOptions = [
         />
       </div>
       <PhotoUpload />
-      <TagsSelect />
+      <TagsSelect @update:selectedTag="handleTagUpdate" />
     </section>
   </section>
 </template>
