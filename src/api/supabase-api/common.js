70f9831ca73dc7a780721aea3efec51dee64583e
kangsuyeong <ekdh16@naver.com>
2025-01-27 11:47:43 +0900

    [FEAT] 댓글 수정 공통함수 추가

--- src/api/supabase-api/common.js
+++ src/api/supabase-api/common.js
@@ -1,19 +1,5 @@
 import { supabase } from "@/supabase";
 
-// // 게시물 댓글 조회
-// export const getComments = async (table, postId) => {
-//   const { data, error } = await supabase
-//     .from(table)
-//     .select("*")
-//     .eq("post_id", postId);
-
-//   if (error) {
-//     console.error("Error fetching free post comments:", error);
-//     return null;
-//   }
-//   return data;
-// };
-
 // 게시물 댓글 조회 (유저 정보, 이미지 추가)
 export const getComments = async (table, postId) => {
   const { data, error } = await supabase
@@ -58,6 +44,30 @@ export const createComment = async (table, memberId, postId, content) => {
 
   if (error) {
     console.error("Error creating free post comment:", error);
+    return null;
+  }
+  return data;
+};
+
+// 게시물 댓글 수정
+export const updateComment = async (table, commentId, content) => {
+  const { data, error } = await supabase
+    .from(table)
+    .update({ content })
+    .eq("id", commentId).select(`
+      id,
+      post_id,
+      content,
+      created_at,
+      member_id,
+      user_info (
+        name,
+        image
+      )
+    `); // 갱신된 데이터와 관련된 user_info를 반환
+
+  if (error) {
+    console.error("Error updating free post comment:", error);
     return null;
   }
   return data;
